5503bde5163ef4f61a9f84c616e7a915f2f51171
Chris Speciale <christopher.gaf@gmail.com>
2020-08-31 00:43:47 -0400

    Added documentation

--- src/actionscape/display/Composite.hx
+++ src/actionscape/display/Composite.hx
@@ -5,6 +5,24 @@ import openfl.display.Sprite;
  * ...
  * @author Christopher Speciale, Dimensionscape LLC
  */
+
+ /**
+The Composite class enables the seamless combination of DisplayTarget 
+wrapped DisplayObjects and DisplayModels within a single parent container
+while minimizing draw calls by batching children branches when possible.
+
+Using the Composite class almost always requires the most draw calls and
+should be avoided if traditional DisplayObjects are unnecessary or can be
+drawn in the nativeForeground or nativeBackground containers.
+
+Traditional DisplayObjects cannot be added or removed directly to and from
+a Composite with the `addChild()` and `removeChild()` methods. There are 2
+ways to handle adding and removing DisplayObjects from a Composite: 1.) Using
+the DisplayTarget wrapped DisplayObjects located in the actionscape.display
+package with the `addChild()` and `removeChild()` methods, or 2.) Adding
+traditional DisplayObjects without a wrapper into the nativeForeground or 
+nativeBackground containers.
+ **/
 class Composite extends DisplayModelContainer 
 {
 	private var __sprite:Sprite;
@@ -23,4 +41,12 @@ class Composite extends DisplayModelContainer
 		return displayModel;
 	}
 	
+	
+	override public function removeChild(displayTarget:DisplayModel):DisplayModel
+	{
+		__children.remove(displayTarget);
+		__tileContainer.removeTile(displayTarget.__tileContainer);
+		__update();
+		return displayTarget;
+	}	
 }
